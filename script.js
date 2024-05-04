96b84e145606201568e57e6a5820e7c556249a44
Meekaylah <kaylahusanga@gmail.com>
2024-05-04 23:26:17 +0100

    Change text

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const editTask = (buttonEl) => {
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
-  
+  addOrUpdateTaskBtn.innerText = "Update Task"
 
 }
 
